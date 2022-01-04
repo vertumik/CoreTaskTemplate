c5d8beb61a1a23333bbe716f45ae7991048ccfe8
Maksim Pantelejev <vertumik@gmail.com>
2022-01-04 19:04:53 +0200

    connect to db

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/first_db";
+    private static final String USER = "root";
+    private static final String PASS = "barri11088";
+
+    static{
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection conn;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASS);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
