52782a692d7d57262da556aa2dfdd2a11573917c
Maksim Pantelejev <vertumik@gmail.com>
2022-01-13 17:36:56 +0200

    add to User class name of table

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.model;
 import javax.persistence.*;
 
 @Entity
-@Table
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
